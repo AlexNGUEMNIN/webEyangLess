41e987de3caabf02fe2f8458d77da815feaaeb38
AlexNGUEMNIN <alexnguemnin@gmail.com>
2025-07-09 16:26:49 +0100

    Remove unused components and styles from the cities, dashboard, map, my-account, settings, and users pages to streamline the application and improve maintainability.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ConnexionComponent } from './pages/admin/connexion/connexion.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { CitiesListComponent } from './pages/cities/cities-list/cities-list.component';
-import { MapComponent } from './pages/map/map.component';
-import { MyAccountComponent } from './pages/my-account/my-account.component';
-import { SettingsComponent } from './pages/settings/settings.component';
-import { UsersComponent } from './pages/users/users.component';
-import { DetailsCitiesComponent } from './pages/cities/details-cities/details-cities.component';
+// import { ConnexionComponent } from './pages/admin/connexion/connexion.component';
+// import { DashboardComponent } from './pages/dashboard/dashboard.component';
+// import { CitiesListComponent } from './pages/cities/cities-list/cities-list.component';
+// import { MapComponent } from './pages/map/map.component';
+// import { MyAccountComponent } from './pages/my-account/my-account.component';
+// import { SettingsComponent } from './pages/settings/settings.component';
+// import { UsersComponent } from './pages/users/users.component';
+// import { DetailsCitiesComponent } from './pages/cities/details-cities/details-cities.component';
 import { LoginComponent } from './pages/utilisateur/authentification/login/login.component';
 import { InscriptionComponent } from './pages/utilisateur/authentification/inscription/inscription.component';
 import { PasswordForgotComponent } from './pages/utilisateur/authentification/password-forgot/password-forgot.component';
@@ -26,15 +26,15 @@ export const routes: Routes = [
         component: AppComponent
     },
 
-    {
-        path : "admin/connxxxion",
-        title: "Connexion",
-        component: ConnexionComponent
-    },
+    // {
+    //     path : "admin/connxxxion",
+    //     title: "Connexion",
+    //     component: ConnexionComponent
+    // },
 
-    { path: 'bailleur', loadChildren: () => import('./pages/bailleur/bailleur.module').then(m => m.BailleurModule) },
+    // { path: 'bailleur', loadChildren: () => import('./pages/bailleur/bailleur.module').then(m => m.BailleurModule) },
     { path: 'user', loadChildren: () => import('./pages/utilisateur/utilisateur.module').then(m => m.UtilisateurModule) },
-    { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule) },
+    // { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule) },
     { path: 'website', loadChildren: () => import('./pages/website/website.module').then(m => m.WebsiteModule)},
 
     // Routes d'authentification
@@ -72,45 +72,45 @@ export const routes: Routes = [
     },
 
     // Routes de l'administrateur
-     {
-        path: '',
-        children: [
-            {
-                path: 'dashboard',
-                title: 'Tableau de bord',
-                component: DashboardComponent
-            },
-            {
-                path: 'cities',
-                title: 'Cités',
-                component: CitiesListComponent
-            },
-            {
-                path: 'details-cites/:id',
-                title: 'Details Cités',
-                component: DetailsCitiesComponent
-            },
-            {
-                path: 'map',
-                title: 'Carte',
-                component: MapComponent
-            },
-            {
-                path: 'users',
-                title: 'Utilisateurs',
-                component: UsersComponent
-            },
-            {
-                path: 'my-account',
-                title: 'Mon compte',
-                component: MyAccountComponent
-            },
-            {
-                path: 'settings',
-                title: 'Paramètres',
-                component: SettingsComponent
-            },
-        ]
-      }
+    //  {
+    //     path: '',
+    //     children: [
+    //         {
+    //             path: 'dashboard',
+    //             title: 'Tableau de bord',
+    //             component: DashboardComponent
+    //         },
+    //         {
+    //             path: 'cities',
+    //             title: 'Cités',
+    //             component: CitiesListComponent
+    //         },
+    //         {
+    //             path: 'details-cites/:id',
+    //             title: 'Details Cités',
+    //             component: DetailsCitiesComponent
+    //         },
+    //         {
+    //             path: 'map',
+    //             title: 'Carte',
+    //             component: MapComponent
+    //         },
+    //         {
+    //             path: 'users',
+    //             title: 'Utilisateurs',
+    //             component: UsersComponent
+    //         },
+    //         {
+    //             path: 'my-account',
+    //             title: 'Mon compte',
+    //             component: MyAccountComponent
+    //         },
+    //         {
+    //             path: 'settings',
+    //             title: 'Paramètres',
+    //             component: SettingsComponent
+    //         },
+    //     ]
+    //   }
 
 ];
