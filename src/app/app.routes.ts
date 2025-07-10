9903b67289b635c75fc5a3c6f8bf1ba31e0e3e72
AlexNGUEMNIN <alexnguemnin@gmail.com>
2025-07-11 00:10:05 +0100

    Remove authentication components and related styles
    - Deleted login component and its associated HTML, SCSS, and spec files.
    - Removed new password component and its associated HTML, SCSS, and spec files.
    - Deleted OTP verification component and its associated HTML, SCSS, and spec files.
    - Removed password forgot component and its associated HTML, SCSS, and spec files.
    - Deleted utilisateur component and its associated HTML, SCSS, and spec files.
    - Removed utilisateur routing module and utilisateur module.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-// import { ConnexionComponent } from './pages/admin/connexion/connexion.component';
-// import { DashboardComponent } from './pages/dashboard/dashboard.component';
-// import { CitiesListComponent } from './pages/cities/cities-list/cities-list.component';
-// import { MapComponent } from './pages/map/map.component';
-// import { MyAccountComponent } from './pages/my-account/my-account.component';
-// import { SettingsComponent } from './pages/settings/settings.component';
-// import { UsersComponent } from './pages/users/users.component';
-// import { DetailsCitiesComponent } from './pages/cities/details-cities/details-cities.component';
-import { LoginComponent } from './pages/utilisateur/authentification/login/login.component';
-import { InscriptionComponent } from './pages/utilisateur/authentification/inscription/inscription.component';
-import { PasswordForgotComponent } from './pages/utilisateur/authentification/password-forgot/password-forgot.component';
-import { OtpVerificationComponent } from './pages/utilisateur/authentification/otp-verification/otp-verification.component';
-import { NewPasswordComponent } from './pages/utilisateur/authentification/new-password/new-password.component';
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'website/accueil',
+        redirectTo: 'website/detail-city',
         pathMatch: 'full'
     },
     {
@@ -33,43 +20,12 @@ export const routes: Routes = [
     // },
 
     // { path: 'bailleur', loadChildren: () => import('./pages/bailleur/bailleur.module').then(m => m.BailleurModule) },
-    { path: 'user', loadChildren: () => import('./pages/utilisateur/utilisateur.module').then(m => m.UtilisateurModule) },
+    // { path: 'user', loadChildren: () => import('./pages/utilisateur/utilisateur.module').then(m => m.UtilisateurModule) },
     // { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule) },
     { path: 'website', loadChildren: () => import('./pages/website/website.module').then(m => m.WebsiteModule)},
 
     // Routes d'authentification
-    {
-      path: '',
-      children: [
-          {
-              path: 'login',
-              title: 'Login',
-              component: LoginComponent
-          },
-          {
-              path: 'register',
-              title: 'Register',
-              component: InscriptionComponent
-          },
 
-          // Mot de passe oublié
-          {
-            path: 'forgot-password',
-            title: 'Mot de passe oublié',
-            component: PasswordForgotComponent
-          },
-          {
-            path: 'otp',
-            title: 'Vérification',
-            component: OtpVerificationComponent
-          },
-          {
-            path: 'new-password',
-            title: 'Nouveau mot de passe',
-            component: NewPasswordComponent
-          },
-      ]
-    },
 
     // Routes de l'administrateur
     //  {
